71a469e8e7393ded06bd33a4e1147bfceb944e80
Unnyan Rohatgi <unnyanr11@gmail.com>
2025-04-06 00:48:42 +0530

    Delete beats-garage-master/beats-garage-master directory

--- beats-garage-master/beats-garage-master/src/App.test.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'; 
-import { render, screen } from "@testing-library/react";  
-import App from "./App";  
-
-test("renders Beats Garage header", () => {  
-  render(<App />);  
-  const headerElement = screen.getByText(/🔥 Beats Garage/i); // Match the header text in your app  
-  expect(headerElement).toBeInTheDocument();  
-});  
